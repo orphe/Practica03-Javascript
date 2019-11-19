b2b06850eb1098fcb9f539f5c5540bf2131b35b4
Rayner <rayner@Rayner-macbook.local>
2019-11-19 12:22:18 -0500

    Organizacion de carpetas/ finalización readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Practica Javascript 
+## Creación y validación de un formulario de registro y una galeria de imagenes
+
+1. Formulario
+     El archivo formulario consta de 3 subcarpetas en las cuales se almacena el estilo css, el archivo de validación del registro y la carpeta index
+     que contiene la pagina html formulario y la validacion php.
+
+    - Carpeta index
+        La carpeta index es donde se almacena la pagina en si.
+
+2. Galería  
+    Del mismo que la carpeta formulario, esta carpeta contiene 3 subcarpetas en las que se almacena los estilos, validacion y la página principal.
+    
--- galeria/Javascript/validacionImagen.js
+++ galeria/Javascript/validacionImagen.js
@@ -1,16 +1,17 @@
 var posActual = 0;
 imagenes = [0];
 
-array = [{ "id": 1, "imagen": "/Imagenes/fondo.jpg" },
-{ "id": 2, "imagen": "/Imagenes/fondo2.jpg" },
-{ "id": 3, "imagen": "/Imagenes/fondo3.jpg" },
-{ "id": 4, "imagen": "/Imagenes/fondo4.jpg" },
-{ "id": 5, "imagen": "/Imagenes/fondo5.jpg" },
-{ "id": 6, "imagen": "/Imagenes/fondo6.jpg" },
-{ "id": 7, "imagen": "/Imagenes/fondo7.jpg" },
-{ "id": 8, "imagen": "/Imagenes/fondo8.jpg" },
-{ "id": 9, "imagen": "/Imagenes/fondo9.jpg" },
-{ "id": 10, "imagen": "/Imagenes/fondo10.jpg" }];
+array = [
+{ "id": 1, "imagen":"/galeria/Imagenes/fondo.jpg" },
+{ "id": 2, "imagen": "/galeria/Imagenes/fondo2.jpg" },
+{ "id": 3, "imagen": "/galeria/Imagenes/fondo3.jpg" },
+{ "id": 4, "imagen": "/galeria/Imagenes/fondo4.jpg" },
+{ "id": 5, "imagen": "/galeria/Imagenes/fondo5.jpg" },
+{ "id": 6, "imagen": "/galeria/Imagenes/fondo6.jpg" },
+{ "id": 7, "imagen": "/galeria/Imagenes/fondo7.jpg" },
+{ "id": 8, "imagen": "/galeria/Imagenes/fondo8.jpg" },
+{ "id": 9, "imagen": "/galeria/Imagenes/fondo9.jpg" },
+{ "id": 10, "imagen": "/galeria/Imagenes/fondo10.jpg" }];
 
 function elegir() {
     for (i = 0; i < 5; i++) {
